c571a072b555116df203fe7700324fcc5f4daa1d
ahmed-oues <ahmed.oueslati6110@gmail.com>
2023-05-26 20:38:44 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
 <?php
-$conn= new mysqli('localhost','root','','quiz_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','sammy','password','quiz_db')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
